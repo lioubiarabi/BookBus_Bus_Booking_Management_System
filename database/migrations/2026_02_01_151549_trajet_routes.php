5b90eb60898d622925cf8d821309adf89fe3c97b
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-02-01 16:25:19 +0100

    add traject_routes migration

---
+++ database/migrations/2026_02_01_151549_trajet_routes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('trajet_routes', function (Blueprint $table) {
+            $table->foreignId('trajetId')->constrained('trajet');
+            $table->foreignId('routeId')->constrained('routes');
+            $table->timestamps();
+
+            $table->primary(['trajetId', 'routeId']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('trajet_routes');
+    }
+};
