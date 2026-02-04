325b7cdb9cfcdb6f1f2ea644cd223545b6cd85d3
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-02-04 12:00:00 +0100

    add the home and trip router

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\SearchController::class, 'index'])->name('home');
+Route::get('/trips', function(){
+    return view('trips');
+});
